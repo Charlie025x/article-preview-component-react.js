b4555b536c9f5a8420724ef429e432b20b567daa
charlie <charliealonso2001@gmail.com>
2021-09-11 15:38:35 -0500

    added post-profile-wrapper component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,5 +1,4 @@
-import avatarMichell from '../images/avatar-michelle.jpg';
-import iconShare from '../images/icon-share.svg';
+import PostProfileWrapper from "../post-profile-wrapper/post-profile-wrapper.component";
 
 import React from "react";
 
@@ -10,40 +9,7 @@ const Card = () => (
         <div className="image-container">
             {/* <img className="header-image" src={drawers} alt="drawers"/> */}
         </div>
-        <div className="post-profile-wrapper">
-            <div className="post-container">
-                <h2>
-                Shift the overall look and feel by adding these wonderful 
-                touches to furniture in your home
-                </h2>
-                <p>
-                Ever been in a room and felt like something was missing? Perhaps 
-                it felt slightly bare and uninviting. I’ve got some simple tips 
-                to help you make any room feel complete.
-                </p>
-            </div>
-    
-            <div className="profile-container">
-                <img className="profile-picture" src={ avatarMichell } alt=""/>
-                <div>
-                    <p className="username">Michelle Appleton</p>
-                    <p className="posted-date">28 Jun 2020</p>
-                </div>
-                <button><img src={ iconShare } alt=""/></button>
-            </div>
-
-            {/* <!-- active sharing state, include with javascript --> */}
-            <div className="profile-container activeShare ">
-                <div>
-                    <p className="share">share</p>
-                    <img className="platform-btn" src="../images/icon-facebook.svg" alt=""/>
-                    <img className="platform-btn" src="../images/icon-twitter.svg" alt=""/>
-                    <img className="platform-btn" src="../images/icon-pinterest.svg" alt=""/>
-                </div>
-                <button><img src="./images/icon-share.svg" alt=""/></button>
-            </div>
-
-        </div>
+        <PostProfileWrapper/>
     </div>
 )
 
---
+++ src/components/post-profile-wrapper/post-profile-wrapper.component.jsx
@@ -0,0 +1,49 @@
+import avatarMichell from '../images/avatar-michelle.jpg';
+import iconShare from '../images/icon-share.svg';
+import iconFacebook from '../images/icon-facebook.svg';
+import iconTwitter from '../images/icon-twitter.svg';
+import iconPinterest from '../images/icon-pinterest.svg';
+
+import React from "react";
+
+import './post-profile-wrapper.styles.scss';
+
+
+const PostProfileWrapper = () => (
+        <div className="post-profile-wrapper">
+            <div className="post-container">
+                <h2>
+                Shift the overall look and feel by adding these wonderful 
+                touches to furniture in your home
+                </h2>
+                <p>
+                Ever been in a room and felt like something was missing? Perhaps 
+                it felt slightly bare and uninviting. I’ve got some simple tips 
+                to help you make any room feel complete.
+                </p>
+            </div>
+    
+            <div className="profile-container">
+                <img className="profile-picture" src={ avatarMichell } alt=""/>
+                <div>
+                    <p className="username">Michelle Appleton</p>
+                    <p className="posted-date">28 Jun 2020</p>
+                </div>
+                <button><img src={ iconShare } alt=""/></button>
+            </div>
+
+            {/* <!-- active sharing state, include with javascript --> */}
+            <div className="activeShare">
+                <div>
+                    <p className="share">share</p>
+                    <img className="platform-btn" src={ iconFacebook } alt=""/>
+                    <img className="platform-btn" src={ iconTwitter } alt=""/>
+                    <img className="platform-btn" src={ iconPinterest } alt=""/>
+                </div>
+                <button><img src={ iconShare } alt=""/></button>
+            </div>
+
+        </div>
+)
+
+export default PostProfileWrapper;
